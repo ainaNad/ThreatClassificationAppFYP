40720f2ae385eed2770157ca96c19f4f1b65593f
ainaNad <128501009+ainaNad@users.noreply.github.com>
2025-01-12 15:54:24 +0800

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -13,6 +13,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import seaborn as sns
 import  matplotlib.pyplot as plt
 
+dt= pd.read_csv('/Users/ainanadhirah/Downloads/Amazon_Consumer.csv')
 df = pd.read_csv('/Users/ainanadhirah/Downloads/annotated.csv')
 
 
